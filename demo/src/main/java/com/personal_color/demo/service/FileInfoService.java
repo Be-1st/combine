05e5bb085989cc1b57d4a1d69113f2c344316e3b
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-05-17 16:20:58 +0900

    Add files via upload
    Service 클래스 추가

---
+++ demo/src/main/java/com/personal_color/demo/service/FileInfoService.java
@@ -0,0 +1,18 @@
+package com.personal_color.demo.service;
+
+import com.personal_color.demo.dao.FileInfoDao;
+import com.personal_color.demo.dto.FileInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileInfoService {
+
+    @Autowired
+    FileInfoDao dao;
+
+    public int insertFile(FileInfoDto dto){
+        return dao.insertFile(dto);
+    }
+
+}
