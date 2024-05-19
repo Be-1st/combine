067e946be865ca5da4eaca68e1ae7e0db8b46944
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-05-19 21:36:58 +0900

    Update FileInfoService.java

--- demo/src/main/java/com/personal_color/demo/service/FileInfoService.java
+++ demo/src/main/java/com/personal_color/demo/service/FileInfoService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class FileInfoService {
 
-    @Autowired
+    @Autowired(required = false)
     FileInfoDao dao;
 
     public int insertFile(FileInfoDto dto){
